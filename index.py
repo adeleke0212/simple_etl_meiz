ff12ee3d36d7af093e71dec3ff71acf1a30a4ced
adeleke0212 <128915740+adeleke0212@users.noreply.github.com>
2023-11-11 11:50:00 +0200

    my entire pipeline from extraction to loading

--- index.py
+++ index.py
@@ -1,6 +1,10 @@
 from transformation import extract_transform_save_to_lake
-from helper import create_bucket, datawarehouse_connector, RAW_SCHEMA, DEV_SCHEMA
+from helper import (create_bucket, datawarehouse_connector,
+                    RAW_SCHEMA, DEV_SCHEMA, s3_path, BUCKET_NAME, ARN_ROLE)
 from sql.create import raw, raw_tables_queries, dev
+from constants import db_tables
+from sql.transform import dev_schema_create_tables
+from sql.insert import insert_into_dev_schema
 
 # Step 1: Create bucket in s3
 try:
@@ -42,3 +46,55 @@ def create_raw_schema_tables():
 
 
 # Step 4: copy from s3 to warehouse raw schema
+# N.B delimiter  not delemeter
+# Good intenet connection, hence time out error
+def copy_from_s3_to_dwh():
+    try:
+        conn = datawarehouse_connector()
+        cursor = conn.cursor()
+        for table in db_tables:
+            print(f"Copying {table} from s3 to dwh")
+            copy_query = f"""
+            copy {RAW_SCHEMA}.{table}
+            from '{s3_path.format(BUCKET_NAME, table)}'
+            iam_role '{ARN_ROLE}'
+            delimiter ','
+            ignoreheader 1;
+            
+            """
+            cursor.execute(copy_query)
+            conn.commit()
+            print(f"{table} copied to dwh")
+        cursor.close()
+        conn.close()
+    except Exception as e:
+        print(e)
+
+# Step 5: Create dev schema...Done
+# Step 6: Create transformed dev schema tables
+
+
+def create_dev_schema_tables():
+    conn = datawarehouse_connector()
+    cursor = conn.cursor()
+    for query in dev_schema_create_tables:
+        print(f"Task: {query[:35]}")
+        cursor.execute(query.format(DEV_SCHEMA))
+        conn.commit()
+    print('All dev schema tables created in datawarehouse')
+    cursor.close()
+    conn.close()
+
+# Step 7: Insert into transformed schema tables
+
+
+def transformed_schema_insertions():
+    conn = datawarehouse_connector()
+    cursor = conn.cursor()
+    for query in insert_into_dev_schema:
+        print(f"Insert Task: {query[:30]}")
+        cursor.execute(query.format(DEV_SCHEMA))
+        conn.commit()
+    print('All insert table jobs done')
+    cursor.close()
+    conn.close()
