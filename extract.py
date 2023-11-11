8257c6ed6d3b6514482c292afd306efb294be634
adeleke0212 <128915740+adeleke0212@users.noreply.github.com>
2023-11-11 11:59:51 +0200

    extract script

--- extract.py
+++ extract.py
@@ -2,7 +2,7 @@ from constants import db_tables
 import pandas as pd
 import os
 from helper import database_connector, create_bucket, sql_alchemy_db_conn
-from sql.copy import copy_query
+from sql.select import copy_query
 
 # seems read_sql_query works well with sqlalchemyconnection rather than psycopg2
 # from the error pandas only supports SQLAlchemy connection
@@ -13,6 +13,3 @@ def extract_from_postgres():
     for table in db_tables:
         df = pd.read_sql_query(copy_query.format(table), con=conn)
         df.to_csv(f'Datasets/{table}.csv', index=False)
-
-
-extract_from_postgres()
